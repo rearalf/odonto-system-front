3ab5d49493dfbedc3985359a5eefde5c7bf2a25e
Ricardo Alf <rear_recinos@hotmail.com>
2026-09-08 09:50:16 -0600

    feat: Integrate Tailwind CSS with Vite and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),

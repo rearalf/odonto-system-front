408ff5605d7cabac7ad53d61a5267fa1b456a8e7
Ricardo Alf <rear_recinos@hotmail.com>
2026-09-16 15:05:28 -0600

    refactor(enum): Change GenderType from enum to const object

--- src/modules/patients/enums/GenderType.ts
+++ src/modules/patients/enums/GenderType.ts
@@ -1,5 +1,7 @@
-export enum GenderType {
-  MALE = 'male',
-  FEMALE = 'female',
-  OTHER = 'other',
-}
+export const GenderType = {
+  MALE: 'male',
+  FEMALE: 'female',
+  OTHER: 'other',
+} as const;
+
+export type GenderType = (typeof GenderType)[keyof typeof GenderType];

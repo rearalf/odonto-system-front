8465f6343189a5299f72d3ca14a27250901cc24a
Ricardo Alf <rear_recinos@hotmail.com>
2026-09-17 07:44:14 -0600

    feat(app): Configurar React Query

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../index.css';
 import App from './App';
+import { ReactQueryProvider } from './providers/ReactQueryProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReactQueryProvider>
+      <App />
+    </ReactQueryProvider>
   </StrictMode>,
 );
